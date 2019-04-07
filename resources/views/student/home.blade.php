6311af6210c5edfecb7d30e330aa26966538e7cb
touhe <touhedulislam46@gmail.com>
2019-04-07 10:38:51 +0600

    check change

--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <div class="card-body">
-        <h4 class="card-title">Welcome to Bup Student Info.</h4>
+        <h4 class="card-title">Welcome to Bup Student Info System.</h4>
     </div>
     <div style="margin-bottom: 335px"></div>
 
